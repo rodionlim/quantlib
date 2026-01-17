46d5b10775a1c47a5b997df09e0c53b9a472d85c
Rodion Lim <rodion.lim@hotmail.com>
2026-01-18 01:15:19 +0800

    feat: add system skeleton with ported accounts

---
+++ src/quantlib_st/sysdata/csv/__init__.py
@@ -0,0 +1,13 @@
+__all__ = [
+    "csvFuturesMultiplePricesData",
+    "csvFuturesAdjustedPricesData",
+    "csvFxPricesData",
+    "csvFuturesInstrumentData",
+    "csvRollParametersData",
+]
+
+from quantlib_st.sysdata.csv.csv_multiple_prices import csvFuturesMultiplePricesData
+from quantlib_st.sysdata.csv.csv_adjusted_prices import csvFuturesAdjustedPricesData
+from quantlib_st.sysdata.csv.csv_spot_fx import csvFxPricesData
+from quantlib_st.sysdata.csv.csv_instrument_data import csvFuturesInstrumentData
+from quantlib_st.sysdata.csv.csv_roll_parameters import csvRollParametersData
